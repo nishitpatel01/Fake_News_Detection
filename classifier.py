b8f081be1f219300c2b7d14b4c1184a4f06540fc
Nishit Patel <nkp3@illinois.edu>
2017-12-05 23:05:34 -0500

    added learning curve

--- classifier.py
+++ classifier.py
@@ -10,6 +10,9 @@ import FeatureSelection
 import numpy as np
 import pandas as pd
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import  LogisticRegression
@@ -17,8 +20,8 @@ from sklearn.linear_model import SGDClassifier
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import KFold
-from sklearn import metrics, confusion_matrix, f1_score
-from sklearn.model_selection import GridSeachCV
+from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import learning_curve
 import matplotlib.pyplot as plt
 
@@ -58,14 +61,14 @@ np.mean(predicted_LogR == DataPrep.test_news['Label'])
 #building Linear SVM classfier
 svm_pipeline = Pipeline([
         ('svmCV',FeatureSelection.countV),
-        ('svm_clf',svm.SVC())
+        ('svm_clf',svm.LinearSVC())
         ])
 
 svm_pipeline.fit(DataPrep.train_news['Statement'],DataPrep.train_news['Label'])
 
 predicted_svm = svm_pipeline.predict(DataPrep.test_news['Statement'])
 np.mean(predicted_svm == DataPrep.test_news['Label'])
-#accuracy = 0.56
+#accuracy = 0.60
 
 
 #using SVM Stochastic Gradient Descent on hinge loss
@@ -133,27 +136,27 @@ build_confusion_matrix(random_forest)
 #Naive bayes
 # [2118 2370]
 # [1664 4088]
-#0.669611539651
+# f1-Score: 0.669611539651
 
 #Logistic regression
 # [2252 2236]
 # [1933 3819]
-# 0.646909097798
+# f1-Score: 0.646909097798
 
 #svm
-# [0 4488]
-# [ 0 5752]
-# 0.719313230705
+# [2260 2228]
+# [2246 3506]
+#f1-score: 0.610468748792
 
 #sgdclassifier
 # [2535 1953]
 # [2502 3250]
-# 0.590245654241
+# f1-Score: 0.590245654241
 
 #random forest classifier
 # [1821 2667]
 # [1192 4560]
-#Score: 0.702651511011
+# f1-Score: 0.702651511011
 #=========================================================================================
 
 count_vect = CountVectorizer()
@@ -161,11 +164,11 @@ X_train_counts = count_vect.fit_transform(DataPrep.train_news['Statement'].value
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
 
+
 """Usinng n-grams, stopwords etc """
 ##Now using n-grams
 #naive-bayes classifier
 nb_pipeline_ngram = Pipeline([
-        #('NBCV',countV_ngram),
         ('nb_tfidf',FeatureSelection.tfidf_ngram),
         ('nb_clf',MultinomialNB())])
 
@@ -177,7 +180,6 @@ np.mean(predicted_nb_ngram == DataPrep.test_news['Label'])
 
 #logistic regression classifier
 logR_pipeline_ngram = Pipeline([
-        #('LogRCV',countV_ngram),
         ('LogR_tfidf',FeatureSelection.tfidf_ngram),
         ('LogR_clf',LogisticRegression(penalty="l2",C=1))
         ])
@@ -191,14 +193,14 @@ np.mean(predicted_LogR_ngram == DataPrep.test_news['Label'])
 #linear SVM classifier
 svm_pipeline_ngram = Pipeline([
         ('svm_tfidf',FeatureSelection.tfidf_ngram),
-        ('svm_clf',svm.SVC())
+        ('svm_clf',svm.LinearSVC())
         ])
 
 svm_pipeline_ngram.fit(DataPrep.train_news['Statement'],DataPrep.train_news['Label'])
 
 predicted_svm_ngram = svm_pipeline_ngram.predict(DataPrep.test_news['Statement'])
 np.mean(predicted_svm_ngram == DataPrep.test_news['Label'])
-#accuracy = 0.56
+#accuracy = 0.62
 
 #sgd classifier
 sgd_pipeline_ngram = Pipeline([
@@ -207,8 +209,6 @@ sgd_pipeline_ngram = Pipeline([
          ])
 
 sgd_pipeline_ngram.fit(DataPrep.train_news['Statement'],DataPrep.train_news['Label'])
-
-sgd_pipeline_ngram.predict(doc_new)
 
 predicted_sgd_ngram = sgd_pipeline_ngram.predict(DataPrep.train_news['Statement'])
 np.mean(predicted_sgd_ngram == DataPrep.train_news['Label'])
@@ -248,9 +248,9 @@ build_confusion_matrix(random_forest_ngram)
 # f1-Score: 0.70113000531
 
 #svm
-# [0 4488]
-# [ 0 5752]
-# f1-Score: 0.719313230705
+# [2016 2472]
+# [1524 4228]
+# f1-Score: 0.67909201429
 
 #sgdclassifier
 # [  10 4478]
@@ -302,7 +302,21 @@ gs_clf.best_score_
 gs_clf.best_params_
 gs_clf.cv_results_
 
-#by running above command we can find the model with best performing parameters
+#Linear SVM 
+parameters = {'svm_tfidf__ngram_range': [(1, 1), (1, 2),(1,3),(1,4),(1,5)],
+               'svm_tfidf__use_idf': (True, False),
+               'svm_tfidf__smooth_idf': (True, False),
+               'svm_clf__penalty': ('l1','l2'),
+}
+
+gs_clf = GridSearchCV(svm_pipeline_ngram, parameters, n_jobs=-1)
+gs_clf = gs_clf.fit(DataPrep.train_news['Statement'][:10000],DataPrep.train_news['Label'][:10000])
+
+gs_clf.best_score_
+gs_clf.best_params_
+gs_clf.cv_results_
+
+#by running above commands we can find the model with best performing parameters
 
 
 #running both random forest and logistic regression models again with best parameter found with GridSearch method
@@ -323,7 +337,6 @@ logR_pipeline_final = Pipeline([
         ])
 
 logR_pipeline_final.fit(DataPrep.train_news['Statement'],DataPrep.train_news['Label'])
-
 predicted_LogR_final = logR_pipeline_final.predict(DataPrep.test_news['Statement'])
 np.mean(predicted_LogR_final == DataPrep.test_news['Label'])
 #accuracy = 0.62
@@ -348,27 +361,118 @@ model_file = 'final_model.sav'
 pickle.dump(logR_pipeline_ngram,open(model_file,'wb'))
 
 
-##np.random.seed(42)
-#data = 
+#Plotting learing curve
+def plot_learing_curve(pipeline,title):
+    size = 10000
+    cv = KFold(size, shuffle=True)
+    
+    X = DataPrep.train_news["Statement"]
+    y = DataPrep.train_news["Label"]
+    
+    pl = pipeline
+    pl.fit(X,y)
+    
+    train_sizes, train_scores, test_scores = learning_curve(pl, X, y, n_jobs=-1, cv=cv, train_sizes=np.linspace(.1, 1.0, 5), verbose=0)
+       
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+     
+    plt.figure()
+    plt.title(title)
+    plt.legend(loc="best")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.gca().invert_yaxis()
+    
+    # box-like grid
+    plt.grid()
+    
+    # plot the std deviation as a transparent range at each training set size
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    
+    # plot the average training and test score lines at each training set size
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+    
+    # sizes the window for readability and displays the plot
+    # shows error from 0 to 1.1
+    plt.ylim(-.1,1.1)
+    plt.show()
 
+
+#below command will plot learing curves for each of the classifiers
+plot_learing_curve(logR_pipeline_ngram,"Naive-bayes Classifier")
+plot_learing_curve(nb_pipeline_ngram,"LogisticRegression Classifier")
+plot_learing_curve(svm_pipeline_ngram,"SVM Classifier")
+plot_learing_curve(sgd_pipeline_ngram,"SGD Classifier")
+plot_learing_curve(random_forest_ngram,"RandomForest Classifier")
 
 """
-#plottig most informative features for random forest
-features = random_forest_ngram.named_steps['rf_clf'].feature_importances_
-std = np.std([tree.feature_importances_ for tree in random_forest_ngram.named_steps['rf_clf'].estimators_],
-             axis=0)
-
-indices = np.argsort(features)#[-20:]
-
-print(train["Statement"].shape[1])
-
-plt.figure()
-plt.title("Feature importances")
-plt.barh(range(train["Statement"].shape[1]), features[indices],
-       color="r", xerr=std[indices], align="center")
-
-plt.yticks(range(train["Statement"].shape[1]), indices)
-plt.ylim([-1, train["Statement"].shape[1]])
-plt.show()
+by plotting the learning cureve for logistic regression, it can be seen that cross-validation score is stagnating throughout and it 
+is unable to learn from data. Also we see that there are high errors that indicates model is simple and we may want to increase the
+model complexity.
 """
 
+"""
+Now let's extract the most informative feature from ifidf vectorizer for all fo the classifiers and see of there are any common
+words that we can identify i.e. are these most informative feature acorss the classifiers are same? we will create a function that 
+will extract top 50 features.
+"""
+
+def show_most_informative_features(model, vect, clf, text=None, n=50):
+    # Extract the vectorizer and the classifier from the pipeline
+    vectorizer = model.named_steps[vect]
+    classifier = model.named_steps[clf]
+
+     # Check to make sure that we can perform this computation
+    if not hasattr(classifier, 'coef_'):
+        raise TypeError(
+            "Cannot compute most informative features on {}.".format(
+                classifier.__class__.__name__
+            )
+        )
+            
+    if text is not None:
+        # Compute the coefficients for the text
+        tvec = model.transform([text]).toarray()
+    else:
+        # Otherwise simply use the coefficients
+        tvec = classifier.coef_
+
+    # Zip the feature names with the coefs and sort
+    coefs = sorted(
+        zip(tvec[0], vectorizer.get_feature_names()),
+        reverse=True
+    )
+    
+    # Get the top n and bottom n coef, name pairs
+    topn  = zip(coefs[:n], coefs[:-(n+1):-1])
+
+    # Create the output string to return
+    output = []
+
+    # If text, add the predicted value to the output.
+    if text is not None:
+        output.append("\"{}\"".format(text))
+        output.append(
+            "Classified as: {}".format(model.predict([text]))
+        )
+        output.append("")
+
+    # Create two columns with most negative and most positive features.
+    for (cp, fnp), (cn, fnn) in topn:
+        output.append(
+            "{:0.4f}{: >15}    {:0.4f}{: >15}".format(
+                cp, fnp, cn, fnn
+            )
+        )
+    #return "\n".join(output)
+    print(output)
+
+show_most_informative_features(logR_pipeline_ngram,vect='LogR_tfidf',clf='LogR_clf')
+show_most_informative_features(nb_pipeline_ngram,vect='nb_tfidf',clf='nb_clf')
+show_most_informative_features(svm_pipeline_ngram,vect='svm_tfidf',clf='svm_clf')
+show_most_informative_features(sgd_pipeline_ngram,vect='sgd_tfidf',clf='sgd_clf')
