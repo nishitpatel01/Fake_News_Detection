dd7af739a514495d8411cbc9ae43419849292b19
Nishit Patel <nkp3@illinois.edu>
2017-12-05 23:34:29 -0500

    code clean up

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -7,14 +7,10 @@ Created on Sat Nov  4 14:13:38 2017
 Note: before we can train an algorithm to classify fake news labels, we need to extract features from it. It means reducing the mass
 of unstructured data into some uniform set of attributes that an algorithm can understand. For fake news detection, it could be 
 word counts (bag of words). 
-
 """
-#from DataPrep.py import *
 import DataPrep
-
 import pandas as pd
 import numpy as np
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -22,7 +18,6 @@ from sklearn.pipeline import Pipeline
 import nltk
 import nltk.corpus 
 from nltk.tokenize import word_tokenize
-
 from gensim.models.word2vec import Word2Vec
 
 
